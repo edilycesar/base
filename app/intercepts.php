ebdd6fc23243fb56d343deaf54f01b4524d9e260
edilycesar <edilycesar@gmail.com>
2016-09-24 18:37:27 -0300

    clean2

--- app/intercepts.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- *  ex1: "UsuarioController@carregar" => "Autenticar@autenticar",  
- *  ex2: "*@*" => "Permissao@permitir"
- *  * = coringa
- */
-return array(    
-    "*@*" => "Permissao@permitir"
-);
