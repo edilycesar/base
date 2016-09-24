d4c4ae1e5c08c89b79bf42d7152da031745e21c6
edilycesar <edilycesar@gmail.com>
2016-09-24 19:39:26 -0300

    Register up

--- src/BaseController.php
+++ src/BaseController.php
@@ -15,51 +15,12 @@ abstract class BaseController
 
     public function __construct()
     {
-        $this->route = Register::get('route');
-        $this->dados = Register::get('dados');
+        $this->route = Edily\base\Register::get('route');
+        $this->dados = Edily\base\Register::get('dados');
     }
 
     public function getParam($key, $default = '')
-    {
-        echo "blah";
+    {        
         return isset($this->route->params[$key]) ? $this->route->params[$key] : $default;
-    }
-
-    public function headAddCss($file)
-    {
-        $filename = PUBLIC_ROOT.'/'.$file;
-        if (!file_exists($filename)) {
-            die('Arquivo não encontrado: '.$filename);
-        }
-        $css = file_get_contents($filename);
-        $css = Cssmin::make($css);
-        $this->headCss .= $css;
-    }
-
-    public function headAddJsFile($file)
-    {
-        $filename = PUBLIC_ROOT.'/'.$file;
-        if (!file_exists($filename)) {
-            die('Arquivo não encontrado: '.$filename);
-        }
-        $script = file_get_contents($filename);
-        $this->headAddJsScript($script);
-    }
-
-    public function headAddJsScript($script)
-    {
-        $jsq = new JSqueeze();
-        $this->headJs .= $jsq->squeeze($script, true, false);
-        //$this->headJs .= $script; //debug
-    }
-
-    public function headGetCss()
-    {
-        return "<style>{$this->headCss}</style>";
-    }
-
-    public function headGetJs()
-    {
-        return  "<script type='text/javascript'>{$this->headJs}</script>";
-    }
+    }    
 }
