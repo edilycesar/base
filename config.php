9db7f10106caafe257f78f2d9446bef13e7ff26a
edilycesar <edilycesar@gmail.com>
2016-09-27 13:41:58 -0300

    Ajuste de config

--- config.php
+++ config.php
@@ -1,11 +1,34 @@
 <?php
-define('PUBLIC_FOLDER', '/public');
-define('APP_ROOT', GL_ROOT . '/app' );
-define('PUBLIC_ROOT', GL_ROOT . PUBLIC_FOLDER );
-define('APP_CONTROLLER_PATH', APP_ROOT . '/controller' );
-define('APP_MODEL_PATH', APP_ROOT . '/model' );
-define('FW_ROOT', GL_ROOT . '/vendor/edily/base/' );
-define('FW_CLASS_PATH', FW_ROOT . '/src/' );
-define('VIEW_PATH', APP_ROOT . '/view' );
 
-define('APP_CONTROLLER_NAMESPACE', 'App\Controller' ); 
+$con = Edily\Base\Config::read();
+
+define('PUBLIC_FOLDER', $con->getPublicFolderName());
+define('APP_ROOT', $con->getAppRoot());
+define('PUBLIC_ROOT', $con->getPublicRoot());
+define('APP_CONTROLLER_PATH', $con->getAppControllerPath() );
+define('APP_MODEL_PATH', $con->getAppModelPath());
+define('FW_ROOT', $con->getFwRoot());
+define('FW_CLASS_PATH', $con->getFwClassPath() );
+define('VIEW_PATH', $con->getViewPath());
+define('APP_CONTROLLER_NAMESPACE', $con->getAppControllerNamespace()); 
+
+//$publicPath = '/public';
+//define('PUBLIC_FOLDER', $publicPath);
+//define('APP_ROOT', GL_ROOT . '/app' );
+//define('PUBLIC_ROOT', GL_ROOT . PUBLIC_FOLDER );
+//define('APP_CONTROLLER_PATH', APP_ROOT . '/controller' );
+//define('APP_MODEL_PATH', APP_ROOT . '/model' );
+//define('FW_ROOT', GL_ROOT . '/vendor/edily/base/' );
+//define('FW_CLASS_PATH', FW_ROOT . '/src/' );
+//define('VIEW_PATH', APP_ROOT . '/view' );
+//define('APP_CONTROLLER_NAMESPACE', 'App\Controller' ); 
+
+echo "<br/>PUBLIC_FOLDER " . PUBLIC_FOLDER;
+echo "<br/>APP_ROOT " . APP_ROOT;
+echo "<br/>PUBLIC_ROOT " . PUBLIC_ROOT;
+echo "<br/>APP_CONTROLLER_PATH " . APP_CONTROLLER_PATH;
+echo "<br/>APP_MODEL_PATH " . APP_MODEL_PATH;
+echo "<br/>FW_ROOT " . FW_ROOT;
+echo "<br/>FW_CLASS_PATH " . FW_CLASS_PATH;
+echo "<br/>VIEW_PATH " . VIEW_PATH;
+echo "<br/>APP_CONTROLLER_NAMESPACE " . APP_CONTROLLER_NAMESPACE;
---
+++ src/Config.php
@@ -0,0 +1,160 @@
+<?php
+namespace Edily\Base;
+
+/**
+ * Description of Config
+ *
+ * @author edily
+ */
+class Config {
+    
+    private $globalRoot;
+    private $publicFolderName;    
+    private $appRoot;    
+    private $appControllerPath;
+    private $appModelPath;
+    private $fwRoot;
+    private $fwClassPath;
+    private $viewPath;
+    private $publicRoot;
+    private $appControllerNamespace;
+
+    public function write() 
+    {
+        $_SESSION['edily_config']['config'] = $this;
+    }
+    
+    public static function read() 
+    {
+        return $_SESSION['edily_config']['config'];
+    }
+    
+    public function getGlobalRoot() 
+    {
+        return !empty($this->globalRoot) ? $this->globalRoot : __DIR__ . "/../../../" ;
+    }
+    
+    public function getPublicFolderName() 
+    {
+        $publicFolder = !empty($this->publicFolderName) 
+                                ? $this->publicFolderName : "public";
+        $publicFolder = "/" . $publicFolder;
+        return str_replace("//", "/", $publicFolder);
+    }
+
+    public function getAppRoot() 
+    {
+        if(!empty($this->appRoot)){
+            return $this->appRoot;
+        }        
+        return $this->getGlobalRoot() . "/app";
+    }
+
+    public function getAppControllerPath() 
+    {
+        return !empty($this->appControllerPath) 
+            ? $this->appControllerPath 
+            : $this->getAppRoot() . '/controller';
+    }
+
+    public function getAppModelPath() 
+    {
+        return !empty($this->appModelPath) 
+            ? $this->appModelPath
+            : $this->getAppRoot() . '/model';
+    }
+
+    public function getFwRoot() 
+    {
+        return __DIR__ . "/../";
+    }
+
+    public function getFwClassPath() 
+    {        
+        return __DIR__;
+    }
+
+    public function getViewPath() 
+    {
+        return !empty($this->viewPath) 
+            ? $this->viewPath
+            : $this->appRoot . '/view';
+        
+    }
+    
+    public function getPublicRoot() 
+    {
+        return !empty($this->publicRoot) 
+            ? $this->publicRoot 
+            : $this->getGlobalRoot() . $this->getPublicFolderName();
+    }
+
+    public function getAppControllerNamespace() 
+    {
+        return !empty($this->appControllerNamespace) 
+            ? $this->appControllerNamespace
+            : 'App\Controller';
+    }
+    
+    public function setGlobalRoot($globalRoot = "") 
+    {
+        $this->globalRoot = $globalRoot;
+    }
+    
+    public function setPublicFolderName($publicFolderName = "public") 
+    {        
+        $this->publicFolderName = $publicFolderName;
+    }
+
+    public function setAppRoot($appRoot = "/app") 
+    {
+        $this->appRoot = $appRoot;
+    }
+
+//    public function setPublicRoot($privateRoot) 
+//    {
+//        $this->privateRoot = $privateRoot;
+//    }
+
+    public function setAppControllerPath($appControllerPath) 
+    {
+        $this->appControllerPath = $appControllerPath;
+    }
+
+    public function setAppModelPath($appModelPath) 
+    {
+        $this->appModelPath = $appModelPath;
+    }
+
+    public function setFwRoot($fwRoot) 
+    {
+        $this->fwRoot = $fwRoot;
+    }
+
+    public function setFwClassPath($fwClassPath) 
+    {
+        $this->fwClassPath = $fwClassPath;
+    }
+
+    public function setViewPath($viewPath) 
+    {
+        $this->viewPath = $viewPath;
+    }
+
+    public function setPublicRoot($publicRoot) 
+    {
+        $this->publicRoot = $publicRoot;
+    }
+    
+}
+//
+//define('PUBLIC_FOLDER', $privatePath);
+//define('APP_ROOT', GL_ROOT . '/app' );
+//define('PUBLIC_ROOT', GL_ROOT . PUBLIC_FOLDER );
+//define('APP_CONTROLLER_PATH', APP_ROOT . '/controller' );
+//define('APP_MODEL_PATH', APP_ROOT . '/model' );
+//define('FW_ROOT', GL_ROOT . '/vendor/edily/base/' );
+//define('FW_CLASS_PATH', FW_ROOT . '/src/' );
+//define('VIEW_PATH', APP_ROOT . '/view' );
+//
+//define('APP_CONTROLLER_NAMESPACE', 'App\Controller' ); 
