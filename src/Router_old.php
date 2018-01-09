ac20a230d073212d7bcda5bb875434787af3b7ea
Cesar <edilycesar@gmail.com>
2018-01-09 09:11:00 -0200

    Passar arction, controller e params via linha de comando

--- src/Router_old.php
+++ src/Router_old.php
@@ -2,66 +2,71 @@
 namespace Edily\Base;
 
 /**
- * Description of router.
+ * Description of router
  *
  * @author edily
  */
-class Router
-{
+class Router {
+    
     protected $requestItens;
     public $controller, $action, $params;
 
-    public function __construct()
-    {
+    public function __construct() 
+    {        
         $this->prepareUri();
         $this->getController();
-        $this->getAction();
+        $this->getAction();   
+
+	//echo "<p>Controller: " . $this->controller . "</p>";
+	//echo "<p>Action: " . $this->action . "</p>"; 
+	//foreach ($this->params as $key => $value) {
+	//	echo "<p>Param: " . $key . " = " . $value . "</p>";
+	//}    
+
         Register::set('route', $this);
     }
-
-    private function getRequestUrI()
+    
+    private function getQueryString() 
     {
-        return $_SERVER['REQUEST_URI'];
+        return $_SERVER['QUERY_STRING'];
     }
-
-    private function getController()
+    
+    private function getController() 
     {
-        $this->controller = !empty($this->requestItens[0]) ? $this->requestItens[0] : 'Index';
+        $this->controller = !empty($this->requestItens[0]) ? $this->requestItens[0] : "Index";
     }
-
-    private function getAction()
+    
+    private function getAction() 
     {
-        $this->action = isset($this->requestItens[1]) ? $this->requestItens[1] : 'index';
+        $this->action = isset($this->requestItens[1]) ? $this->requestItens[1] : "index";
     }
-
-    private function prepareUri()
+    
+    private function prepareUri() 
     {
-        $uri = $this->getRequestUrI();
-        $uriAP = $this->getAfterPublic($uri);
+	$uriAP = $this->getAfterPublic();
         $this->splitRequest($uriAP);
         $this->prepareParams();
     }
 
-    private function getAfterPublic($uri)
-    {
-        $uriPublicPos = 7 + strpos($uri, 'public/');
-
-        return substr($uri, $uriPublicPos);
+    private function getAfterPublic() 
+    {   
+	$pathArr = explode("=", $this->getQueryString());
+        return isset($pathArr[1]) ? $pathArr[1] : '';
     }
-
-    private function splitRequest($uriAP)
+    
+    private function splitRequest($uriAP) 
     {
-        $this->requestItens = explode('/', $uriAP);
+        $this->requestItens = explode("/", $uriAP);
     }
-
-    private function prepareParams()
-    {
-        $var = $val = '';
+    
+    private function prepareParams() 
+    {        
+        $var = $val = "";
         foreach ($this->requestItens as $key => $value) {
-            if ($key > 1) {
-                if (($key % 2) == 0) {
+            if($key > 1){
+                if(($key % 2) == 0){
                     $var = $value;
-                } else {
+                }else{
                     $val = $value;
                 }
                 $this->params[$var] = $val;
