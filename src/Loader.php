ffce21d7893bd564f244a63c1826c25499d7d517
Edily Cesar Medule <edilycesar@gmail.com>
2018-10-10 16:45:11 -0300

    Set controller sufix (Rollback)

--- src/Loader.php
+++ src/Loader.php
@@ -12,27 +12,16 @@ class Loader extends Intercept
 
     protected $route;
     public $obj;
-    private $controllerSufixName;
 
     public function __construct()
     {
         $this->route = Register::get('route');
     }
 
-    public function setControllerSufixName($controllerSufixName)
-    {
-        $this->controllerSufixName = $controllerSufixName;
-        return $this;
-    }
-
     public function loadControllerAction()
     {
         $controllerName = ucfirst($this->route->controller) . '';
-
-        $controllerFile = APP_CONTROLLER_PATH . '/' . $controllerName;
-        $controllerFile .= !empty($this->controllerSufixName) ? $this->controllerSufixName : '';
-        $controllerFile .= '.php';
-
+        $controllerFile = APP_CONTROLLER_PATH . '/' . $controllerName . '.php';
         if (!file_exists($controllerFile)) {
             die('Controller não encontrado: ' . $controllerFile);
         } else {
